ac13c4fb5470a1144ecd027026314b89ce6e75cc
zamuuu <samuelcorreavallejo@gmail.com>
2024-07-03 01:53:30 -0300

    Added POST method to my API

--- users.py
+++ users.py
@@ -17,28 +17,29 @@ user_list = [
             User(id=2,name="Taton",surname="Rossa",url="https://www.tatonelcrack.com",age=18),
             User(id=3,name="Maria",surname="Masardi",url="https://www.mariamasar.com",age=28),]
 
-@app.get("/usersjson")
-async def usersjson():
-    return [{"id": 1,"name": "Coco", "surname": "Alvadez", "url": "https://www.youtube.com", "age": 34},
-            {"id": 2,"name": "Taton", "surname": "Rossa", "url": "https://www.tatonelcrack.com", "age": 18},
-            {"id": 3,"name": "Maria", "surname": "Masardi", "url": "https://www.mariamasar.com", "age": 28},
-            ]
-    
 @app.get("/users")
-async def user():
+async def users():
     return user_list
 
-# path
+# Path
 @app.get("/user/{id}")
 async def user(id: int):
     return search_user(id)
     
-# query
-@app.get("/userquery")
+# Query
+@app.get("/user")
 async def user(id: int):
     return search_user(id)
 
 
+# POST method in my API
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "Ya existe este usuario."}
+    else:
+        return user_list.append(user)
+    
 
 def search_user(id):
     user = filter(lambda user_of_list: user_of_list.id == id, user_list)
